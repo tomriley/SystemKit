bcc76649ab1d38f83c6fb499580db2ec9170d764
beltex <beltex@users.noreply.github.com>
2014-12-27 09:57:43 -0500

    Update README. Add jazzy.sh via SMCKit

---
+++ jazzy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# WHAT: Script to generate API document using jazzy (v0.0.17)
+
+AUTHOR=beltex
+AUTHOR_URL=https://github.com/beltex/
+MODULE_NAME=SystemKit
+GITHUB_URL=http://github.com/beltex/SystemKit
+
+jazzy -a $AUTHOR -u $AUTHOR_URL -m $MODULE_NAME -g $GITHUB_URL
