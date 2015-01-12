742e9a3da140a1f96ff36cbe293d508998c39dd2
beltex <beltex@users.noreply.github.com>
2015-01-12 11:32:31 -0500

    Generate API doc via jazzy (v0.0.20)
    - Applied small patch seen here in link below to fix font for code (to Menlo)
    
      https://github.com/realm/jazzy/issues/137#issuecomment-68619733

--- jazzy.sh
+++ jazzy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# WHAT: Script to generate API document using jazzy (v0.0.17)
+# WHAT: Script to generate API document using jazzy (v0.0.20)
 
 AUTHOR=beltex
 AUTHOR_URL=https://github.com/beltex/
 MODULE_NAME=SystemKit
 GITHUB_URL=http://github.com/beltex/SystemKit
+ACL=public
 
-jazzy -a $AUTHOR -u $AUTHOR_URL -m $MODULE_NAME -g $GITHUB_URL
+jazzy -a $AUTHOR -u $AUTHOR_URL -m $MODULE_NAME -g $GITHUB_URL --min-acl $ACL 
